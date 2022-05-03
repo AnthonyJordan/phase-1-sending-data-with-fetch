59dba269abc1c1568ececb1b19ad1649c035a3b5
Anthony <Kain235@gmail.com>
2022-05-03 13:05:59 -0400

    commit tree 680ae846fc054a042be80153e0817769f5be1d8f

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+
+    const formData = {
+        name: userName,
+        email: userEmail,
+    };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+
+    return fetch('http://localhost:3000/users', configurationObject).then(respone => respone.json()).then(json => document.querySelector('body').append(json.id)).catch(error => document.querySelector('body').append(error.message))
+
+}
